9831e96753de15e31ee368d0f97607a076c63028
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-08-29 08:55:51 +0200

    Up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Hello World! I'm a component!"/>
+    <h1>When a style tag has the scoped attribute,</h1>
+    <h1>its CSS will apply to elements of the current component only.</h1>
   </div>
 </template>
 
@@ -19,11 +21,8 @@ export default class App extends Vue {}
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  font-family: "Courier New", Courier, monospace;
 }
 </style>
